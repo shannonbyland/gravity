391dd886b5c7640874e63f73774fa15abce1d80e
Shannon Byland <shannonbyland@gmail.com>
2017-08-16 18:34:07 -0600

    added home and about pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Auth from './components/Auth';
 
 const App = () => (
-  <p>
-    To get started, edit <code>src/App.js</code> and save to reload.
-  </p>
+  <div>
+    <NavBar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/register" render={ (props) => <Auth {...props} title="Register" /> } />
+      <Route path="/login" render={ (props) => <Auth {...props} title="Login" /> } />
+    </Switch>
+  </div>
 );
 
 export default App;
