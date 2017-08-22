7d2a15bac27f72b116b4abc1a5d3fbf24adb8d6b
Shannon Byland <shannonbyland@gmail.com>
2017-08-22 17:56:36 -0600

    working on about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,7 +2,45 @@ import React from 'react';
 import { Header } from 'semantic-ui-react';
 
 const About = () => (
-  <Header as="h3">This is the about page</Header>
+  <Header as="h3">
+    Gravity Logic has been founded by the team behind the Whistler Mountain Bike Park’s success.
+    In fact, each Gravity Logician is still actively involved in the continuing development of Whistler’s Park.
+    Clearly not a bunch of out of touch consultants.
+
+    Director Dave Kelly is the genius behind A-Line, Dirt Merchant and Whistler’s legendary “flow”.
+    He has been involved with the Whistler Mountain Bike Park since 1996 and was among the first to
+    officially establish downhill mountain bike trails on Whistler Mountain. As the park grew,
+    Dave worked his way up from lead hand to co-manager of the bike park. In 2007, Dave started working
+    with Gravity Logic offering trail development advice to resorts throughout North America and Europe.
+    Dave also has 11 years experience with the Whistler Mountain Ski Patrol bringing experience
+    in risk management, safety, and construction best practices to Gravity Logic’s portfolio.
+
+    Director Tom “Pro” Prochazka managed the Whistler Mountain Bike Park from 2001 to 2007. His ability
+    to visualize trails and trail features that are both progressive and safe has earned him international
+    respect from casual park visitors and professional riders. Tom’s 20 years of experience as a carpenter
+    and sawyer are an incredibly valuable asset when designing trail features that are structurally sound,
+    safe, and fun to ride.
+
+    Director Rob Cocquyt worked on design and construction of the first trails on Whistler Mountain in 1995.
+    In 1996, he established his own trail design and construction company and, over the following years,
+    secured government funding and hired crews to build some of Squamish’s most highly regarded trails.
+    Rob worked full-time with the Whistler Mountain Bike Park from 2005-2008 as lead hand, carpenter, and
+    finally as trail crew supervisor. Since 2007, Rob has traveled the globe with Gravity Logic helping design,
+    develop and build some of the most successful bike parks in the world.
+
+    Business Development –Paul Mutch is a 20-year ski industry veteran; he has filled many senior executive
+    roles, and developed and lead multi-million dollar resort retail and rental businesses in the most
+    competitive marketplaces in North America. He is a strategic thinker with an unmatched track record
+    of innovation and revenue generation. Starting his mountain bike career as a guide in Whistler in 1991,
+    Paul has witnessed first-hand the evolution of mountain biking and developed rental, retail, programming
+    and sponsorship programs for the two leading bike parks in the world; Whistler Bike Park, B.C.
+    and Trestle Bike Park, Colorado. Paul oversees business development for Gravity Logic and our
+    efforts to maximize the financial returns for our client’s bike park investments.
+
+    The Gravity Logic core is assisted by a variety of consultants and contractors on an “as needed” basis.
+    We have direct access to experts in Risk Management, Mapping, Environmental Planning, Rental/Retail,
+    Event Planning and many other areas.
+  </Header>
 )
 
 export default About;
