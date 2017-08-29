757baeb5c343e066102941c0e1b097dc4bfcfca8
Shannon Byland <shannonbyland@gmail.com>
2017-08-28 20:44:04 -0600

    added services page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
+import Services from './components/Services';
 import Auth from './components/Auth';
 import Dashboard from './components/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -15,6 +16,7 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/services" component={Services} />
         <ProtectedRoute path="/dashboard" component={Dashboard} />
         <Route path="/register" render={ (props) => <Auth {...props} title="Register" /> } />
         <Route path="/login" render={ (props) => <Auth {...props} title="Login" /> } />
---
+++ client/src/components/Services.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Header } from 'semantic-ui-react';
+
+
+Our Services
+
+Design and Planning
+A GPS, an important tool in trail design and inventory, sits on a map.Gravity Logic will undertake a thorough site analysis which will result in:
+
+Trail inventory and basic trail mapping
+Terrain inspection and feasibility assessment
+Construction/development costs and ROI
+Lift Requirements
+Operating Plan
+Staffing Plan
+Marketing Plan – including visit and revenue forecasts
+Five Year Financial Plan
+
+export default connect(mapStateToProps)(Services);
