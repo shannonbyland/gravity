e74cb7babcd26aa73c241a4cf2b66fd461830a07
Shannon Byland <shannonbyland@gmail.com>
2017-08-30 18:08:14 -0600

    added services page

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -2,19 +2,35 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Header } from 'semantic-ui-react';
 
+const Services = () => (
+  <div>
+    <Header as="h3">
+      Our Services
+    </Header>
+    <p>
+      Design and Planning
+      A GPS, an important tool in trail design and inventory, sits on a map.Gravity Logic will undertake a thorough site analysis which will result in:
 
-Our Services
+      Trail inventory and basic trail mapping
+      Terrain inspection and feasibility assessment
+      Construction/development costs and ROI
+      Lift Requirements
+      Operating Plan
+      Staffing Plan
+      Marketing Plan – including visit and revenue forecasts
+      Five Year Financial Plan
 
-Design and Planning
-A GPS, an important tool in trail design and inventory, sits on a map.Gravity Logic will undertake a thorough site analysis which will result in:
+      Development and Construction
+      wooden bike trail feature design crank it up whistlerGravity Logic can oversee the construction of the facility as per the Design Plan. This will include:
 
-Trail inventory and basic trail mapping
-Terrain inspection and feasibility assessment
-Construction/development costs and ROI
-Lift Requirements
-Operating Plan
-Staffing Plan
-Marketing Plan – including visit and revenue forecasts
-Five Year Financial Plan
+      Project management plan
+      Detailed Construction Budget
+      Trail Design
+      Trail Construction
+      Maintenance Budget and Schedule
+      Future Development Identification
+    </p>
+ </div>
+)
 
 export default connect(mapStateToProps)(Services);
